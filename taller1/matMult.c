be6dd4ab0cd091bdf0b16955b2a4cecd94c01c6e
Wilson Arce <wdarcedev@gmail.com>
2017-08-07 08:54:00 -0500

    taller matrices

--- taller1/matMult.c
+++ taller1/matMult.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 512
-
 int main (int argc,char **argv)
 {
 	FILE *f1, *f2, *f3;
@@ -10,31 +8,36 @@ int main (int argc,char **argv)
 	//variables para almacenar las dimensiones de las matrices
 	int m1Col, m1Row, m2Col, m2Row;
 
-	//variables para almacenar valores y rerultado
-	int a, b, ans;
-
 	f1 = fopen("mat1.txt","r");
 	f2 = fopen("mat2.txt","r");
 	f3 = fopen("answer.txt","w");
 	
+	//lectura de dimensiones
 	fscanf(f1, "%d", &m1Row); fscanf(f1, "%d", &m1Col);
 	fscanf(f2, "%d", &m2Row); fscanf(f2, "%d", &m2Col);
 
+	//condicion de multiplicidad de las matrices
 	if (m1Col == m2Row)
 	{
+		/*asignacion de memoria para los vectores principales
+		de cada matriz*/
 		int	**mat1;
 		mat1 = (int **)malloc(m1Row * sizeof(int *));
 
 		int	**mat2;
 		mat2 = (int **)malloc(m2Row * sizeof(int *));
 
+
+		/*asignacion de memoria para los vectores que conformaran
+		las columnas y almacenamiento de valores desde el archivo
+		de texto*/
 		for (int i = 0; i < m1Row; i++)
 		{
 			mat1[i] = (int *)malloc(m1Col * sizeof(int));
 			for (int j = 0; j < m1Col; j++)
 			{
 				fscanf(f1, "%d", &mat1[i][j]);
-				getc(f1);
+				getc(f1);//saltar las comas (,)
 			}
 		}
 
@@ -47,11 +50,14 @@ int main (int argc,char **argv)
 				getc(f2);
 			}
 		}
-		
+
+		/*dimensiones de la matriz resultado*/
 		fprintf(f3, "%d\n", m1Row);
 		fprintf(f3, "%d\n", m2Col);
 
-		int a,b;
+		int a,b;/*variables para almacenar valores a multiplicar*/
+
+		/*ciclo para relizar la multiplicacion de matrices */
 		for (int i = 0; i < m1Row; i++) {
 	    for (int j = 0; j < m2Col; j++) {
 	   		int sum = 0;
@@ -62,16 +68,21 @@ int main (int argc,char **argv)
 	      }
 	      fprintf(f3, "%d,", sum);
 	  	}
+
+	  	/*devuelve una posicion al puntero del archivo resultado,
+	  	para no mostar la coma al final de la linea*/
 	  	fseek(f3, -1, SEEK_END);
+
 	  	fprintf(f3, "\n");
 	 	}
 
 		fclose(f1); fclose(f2);
 
+		/*liberacion de memoria*/
 		free(mat1); free(mat2);
 
 	}else{
-		printf("Las matrices no se puede multiplicar\n");
+		printf("Las matrices no se pueden multiplicar\n");
 	}
 
 	return 0;
