9cae635aa30c9db3a70091e2687f42e392bee356
Wilson Arce <wdarcedev@gmail.com>
2017-09-25 16:39:09 -0500

    ocultando multiplicacion cpu

--- myCUDA/cudaexamples/matmult/matmult.cu
+++ myCUDA/cudaexamples/matmult/matmult.cu
@@ -2,17 +2,17 @@
 #include <time.h>
 #define N 512
 
-void Matriz_CPU_Mult(int **h_a, int **h_b, int **h_c) {
+void Matriz_CPU_Mult(int A[N][N], int B[N][N], int C[N][N]) {
 	int n,m;
 	for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
    		int sum = 0;
       for (int k = 0; k < N; k++) {
-        m = h_a[i][k];
-        n = h_b[k][j];
+        m = A[i][k];
+        n = B[k][j];
         sum += m * n;
       }
-   	h_c[i][j] = sum;
+   	C[i][j] = sum;
   	}
  	}
 }
@@ -31,32 +31,28 @@ __global__ void Matriz_GPU_Mult(int *a, int *b, int *c) {
 
 int main() {
   double timeGPU; //, timeCPU;
-	int **h_a, **h_b, **h_c;
+	int A[N][N], B[N][N], C[N][N];
  	int *d_a, *d_b, *d_c;
  	int cont,i,j;
-
-  int size = N * sizeof(int);
-
-  h_a = (int**)malloc(size);
-  h_b = (int**)malloc(size);
-  h_c = (int**)malloc(size);
-
-  cudaMalloc((void **) &d_a, size);
-  cudaMalloc((void **) &d_b, size);
-  cudaMalloc((void **) &d_c, size);
 
   //inicializacion
 	for (i = 0; i < N; i++) {
   	cont = 0;
   	for (j = 0; j < N; j++) {
-   		h_a[i][j] = cont;
-   		h_b[i][j] = cont;
+   		A[i][j] = cont;
+   		B[i][j] = cont;
    		cont++;
   	}
   }
 
-  cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice);
- 	cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice);
+  int size = N * sizeof(int);
+
+	cudaMalloc((void **) &d_a, size);
+ 	cudaMalloc((void **) &d_b, size);
+ 	cudaMalloc((void **) &d_c, size);
+
+  cudaMemcpy(d_a, A, size, cudaMemcpyHostToDevice);
+ 	cudaMemcpy(d_b, B, size, cudaMemcpyHostToDevice);
 
   //int threadsPerBlock(16);
   //int numBlocks(N/threadsPerBlock);
@@ -67,17 +63,13 @@ int main() {
   Matriz_GPU_Mult<<<numBlocks, threadsPerBlock>>>(d_a, d_b, d_c);
 	timeGPU = ((double)(clock() - startGPU))/CLOCKS_PER_SEC;
   
-  cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost);
+  cudaMemcpy(C, d_c, size, cudaMemcpyDeviceToHost);
 	
   /*
   clock_t startCPU = clock();
   Matriz_CPU_Mult(A, B, C);
 	timeCPU = ((double)(clock() - startCPU))/CLOCKS_PER_SEC;
   */
-
-  free(h_a);
-  free(h_b);
-  free(h_c);
 
   cudaFree(d_a);
  	cudaFree(d_b);
