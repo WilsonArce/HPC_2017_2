46c064e5826e1a8eb8982d49f49a0c44e45b14c2
Wilson Arce <wdarcedev@gmail.com>
2017-09-25 16:33:46 -0500

    ocultando multiplicacion cpu

--- myCUDA/cudaexamples/matmult/matmult.cu
+++ myCUDA/cudaexamples/matmult/matmult.cu
@@ -2,7 +2,7 @@
 #include <time.h>
 #define N 512
 
-void Matriz_CPU_Mult(int *h_a, int *h_b, int *h_c) {
+void Matriz_CPU_Mult(int **h_a, int **h_b, int **h_c) {
 	int n,m;
 	for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
