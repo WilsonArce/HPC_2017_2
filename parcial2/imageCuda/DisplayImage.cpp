7732f97bcf1df1164b8cb5fd8465ab181a4c6bc5
WilsonArce <wdarcedev@gmail.com>
2017-10-07 12:01:20 -0500

    new files generate

--- parcial2/imageCuda/DisplayImage.cpp
+++ parcial2/imageCuda/DisplayImage.cpp
@@ -2,7 +2,6 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
-using namespace std;
 
 int main(int argc, char** argv )
 {
@@ -12,13 +11,7 @@ int main(int argc, char** argv )
         return -1;
     }
 
-    Mat image, image_gray;
-		Mat grad;
-		//char* window_name = "Sobel Demo - Simple Edge Detector";
-		int scale = 1;
-		int delta = 0;
-		int ddepth = CV_16S;
-
+    Mat image;
     image = imread( argv[1], 1 );
 
     if ( !image.data )
@@ -27,31 +20,9 @@ int main(int argc, char** argv )
         return -1;
     }
     //namedWindow("Display Image", WINDOW_AUTOSIZE );
+    //imshow("Display Image", image);
 
-    cvtColor( image, image_gray, CV_BGR2GRAY );
-
-    /// Generate grad_x and grad_y
-		Mat grad_x, grad_y;
-		Mat abs_grad_x, abs_grad_y;
-
-		/// Gradient X
-		//Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
-		Sobel( image_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-		convertScaleAbs( grad_x, abs_grad_x );
-
-		/// Gradient Y
-		//Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
-		Sobel( image_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
-		convertScaleAbs( grad_y, abs_grad_y );
-
-		/// Total Gradient (approximate)
-		addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
-
-    imwrite("lena_out.jpg", grad);
-
-    //imshow("Display Image", grad);
-
-    cout << image.size() << endl;
+		imwrite("lenal_out.jpg", image);
 
     //waitKey(0);
 
