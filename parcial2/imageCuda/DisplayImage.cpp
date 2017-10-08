e7ddffcec4d46dd5d26414142b13cc2823100d56
Wilson Arce <wdarcedev@gmail.com>
2017-10-08 15:33:11 -0500

    grayscale function

--- parcial2/imageCuda/DisplayImage.cpp
+++ parcial2/imageCuda/DisplayImage.cpp
@@ -23,8 +23,30 @@ int main(int argc, char** argv )
   //namedWindow("Display Image", WINDOW_AUTOSIZE );
   //imshow("Display Image", image);
 
-  imwrite("lena_out.jpg", image);
-  cout << image << endl;
+  Mat img = image;
+  float r,g,b;
+  for(int y=0;y<image.rows;y++){
+    for(int x=0;x<image.cols;x++){
+      // get pixel
+      Vec3b color = img.at<Vec3b>(Point(x,y));
+
+      r = color[0];
+      g = color[1];
+      b = color[2];
+
+      //I = .299f * R + .587f * G + .114f * B
+      color[2] = (r * 0.299 + g * 0.587 + b * 0.114);
+      color[1] = (r * 0.299 + g * 0.587 + b * 0.114);
+      color[0] = (r * 0.299 + g * 0.587 + b * 0.114);
+
+      // set pixel
+      img.at<Vec3b>(Point(x,y)) = color;
+    }
+  }
+
+  imwrite("lena_out.jpg", img);
+
+  //cout << image << endl;
 
   //waitKey(0);
 
