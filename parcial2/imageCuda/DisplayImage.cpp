be3c0da688554b5f78dc4e53e3d95e528020e7f7
Wilson Arce <wdarcedev@gmail.com>
2017-10-08 11:00:46 -0500

    VS CODE test

--- parcial2/imageCuda/DisplayImage.cpp
+++ parcial2/imageCuda/DisplayImage.cpp
@@ -5,26 +5,27 @@ using namespace cv;
 
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
+  if ( argc != 2 )
+  {
+    printf("usage: DisplayImage.out <Image_Path>\n");
+    return -1;
+  }
 
-    Mat image;
-    image = imread( argv[1], 1 );
+  Mat image;
+  image = imread( argv[1], 1 );
 
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    //namedWindow("Display Image", WINDOW_AUTOSIZE );
-    //imshow("Display Image", image);
+  if ( !image.data )
+  {
+    printf("No image data \n");
+    return -1;
+  }
+  //namedWindow("Display Image", WINDOW_AUTOSIZE );
+  //imshow("Display Image", image);
 
-		imwrite("lena_out.jpg", image);
+  imwrite("lena_out.jpg", image);
+  cout << inage << endl;
 
-    //waitKey(0);
+  //waitKey(0);
 
-    return 0;
+  return 0;
 }
