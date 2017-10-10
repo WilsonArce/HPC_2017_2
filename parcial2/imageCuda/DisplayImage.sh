13f169af029b6e80b507c1599c6442fd257ab537
Wilson Arce <wdarcedev@gmail.com>
2017-10-09 20:37:04 -0500

    new gpugrayscale version

--- parcial2/imageCuda/DisplayImage.sh
+++ parcial2/imageCuda/DisplayImage.sh
@@ -9,6 +9,6 @@
 export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 ./DisplayImage_cu lena.jpg
