7223e06ea02470d80c3d2a17ecc4f57432b87294
Wilson Arce <wdarcedev@gmail.com>
2017-10-16 10:39:00 -0500

    sobel filter cuda implementation

--- parcial2/imageCuda/sobelFilter.cu
+++ parcial2/imageCuda/sobelFilter.cu
@@ -193,12 +193,12 @@ int main(int argc, char** argv )
   imageOut.data = h_imageSobel;
 
   cout<<"Image size = "<< image.size() << endl;
-  printf("Grayscale time : \n");
-  printf("CPU = %f s\n",timeCPU_GS);
-  printf("GPU = %f s\n",timeGPU_GS);
-  printf("Sobel filter time : \n");
-  printf("CPU = %f s\n",timeCPU_SB);
-  printf("GPU = %f s\n",timeGPU_SB);
+  printf("Grayscale time\n");
+  printf("  CPU = %f s\n",timeCPU_GS);
+  printf("  GPU = %f s\n",timeGPU_GS);
+  printf("Sobel filter time\n");
+  printf("  CPU = %f s\n",timeCPU_SB);
+  printf("  GPU = %f s\n",timeGPU_SB);
 
   imwrite("images/imageSobel_gpu.jpg", imageOut);
   imwrite("images/imageSobel_opCV.jpg", grad);
