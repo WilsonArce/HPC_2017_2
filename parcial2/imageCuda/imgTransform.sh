42590e1239ccfee0b6004286c8b4bf011820e49f
Wilson Arce <wdarcedev@gmail.com>
2017-10-17 21:04:19 -0500

    constant memory implementation

--- parcial2/imageCuda/imgTransform.sh
+++ parcial2/imageCuda/imgTransform.sh
@@ -12,6 +12,3 @@ export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRAR
 export CUDA_VISIBLE_DEVICES=0
 
 ./sobelFilterGCM images/image1k.jpg
-./sobelFilterGCM images/image1k_2.jpg
-./sobelFilterGCM images/image4k.jpg
-./sobelFilterGCM images/image8k.jpg
