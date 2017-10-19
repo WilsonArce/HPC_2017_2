dd1b08c20936c283a92412ca82e1444f73fa518d
Wilson Arce <wdarcedev@gmail.com>
2017-10-19 18:19:48 -0500

    shared memory implementation

--- parcial2/imageCuda/sobel_GCSmem.cu
+++ parcial2/imageCuda/sobel_GCSmem.cu
@@ -175,7 +175,7 @@ int main(int argc, char** argv )
   cudaMemcpy(d_imageIn, h_imageIn, imgInSize, cudaMemcpyHostToDevice);
 
   //parameters definition for CUDA kernel
-  int threads = 64;
+  int threads = 16;
   dim3 numThreads(threads, threads);
   dim3 blockDim(ceil(cols/float(threads)), ceil(rows/float(threads)));
 
