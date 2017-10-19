e39e4e825e90cb184d92c2957f56b10ad000d661
Wilson Arce <wdarcedev@gmail.com>
2017-10-18 19:52:06 -0500

    shared memory implementation

--- parcial2/imageCuda/sobel_GCSmem.cu
+++ parcial2/imageCuda/sobel_GCSmem.cu
@@ -38,8 +38,7 @@ __global__ void gpuSobelFilter(unsigned char *imgGray, unsigned char *imgFiltere
 				cj = j-1;
 				for(x = 0; x < sbCols; x++){
 					if(ci < 0 || cj < 0){
-						sumx += 0;
-						sumy += 0;
+						continue;
 					}else{
 						sumx += imgGray[ci * cols + cj] * d_xFilter[y * sbCols + x];
 						sumy += imgGray[ci * cols + cj] * d_yFilter[y * sbCols + x];
