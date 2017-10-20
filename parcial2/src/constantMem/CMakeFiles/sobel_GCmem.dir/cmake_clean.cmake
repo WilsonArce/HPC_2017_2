14f2b0cf81df5301a7cb826fe6f6c4d330f4cc6e
WilsonArce <wdarcedev@gmail.com>
2017-10-20 07:44:09 -0500

    constant mem test

---
+++ parcial2/src/constantMem/CMakeFiles/sobel_GCmem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sobel_GCmem.dir/sobel_GCmem_generated_sobel_GCmem.cu.o"
+  "sobel_GCmem.pdb"
+  "sobel_GCmem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/sobel_GCmem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
